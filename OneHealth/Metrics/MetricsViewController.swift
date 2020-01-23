d328730d0c723739042f3a548f2f007ee1a6b939
Hunter Kemeny <hunterkemeny@Hunters-iMac.lan>
2020-01-22 21:16:30 -0700

    Setup Storyboard

---
+++ OneHealth/Metrics/MetricsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MetricsViewController.swift
+//  OneHealth
+//
+//  Created by Hunter Kemeny on 1/22/20.
+//
+
+import UIKit
+
+class MetricsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
