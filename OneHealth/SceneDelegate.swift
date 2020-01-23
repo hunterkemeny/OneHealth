fe8ed956f2abaa158ea0eb4385ce682f461e52a1
Hunter Kemeny <hunterkemeny@Hunters-iMac.lan>
2020-01-23 14:59:08 -0700

    Make TabViewController Initial ViewController

--- OneHealth/SceneDelegate.swift
+++ OneHealth/SceneDelegate.swift
@@ -11,12 +11,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        /* If user has signed in previously, make TabViewController the initial ViewController. Otherwise, make BaseViewController the initial ViewController. */
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            if(UserDefaults.standard.value(forKey: "email") as? String) == nil {
+                window.rootViewController = storyboard.instantiateViewController(withIdentifier: "BaseViewController")
+            } else {
+                window.rootViewController = storyboard.instantiateInitialViewController()
+            }
+            
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
